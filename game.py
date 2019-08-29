51ad1519e26a15d954068167e1a29f4d904e0a2f
Charles Cavanaugh <charles@Charless-MacBook.local>
2019-08-29 05:41:12 -0500

    added color function

--- game.py
+++ game.py
@@ -10,6 +10,17 @@
 
 import pygame
 import random
+import colorsys
+
+
+def get_N_HexCol(N=5):
+    HSV_tuples = [(x * 1.0 / N, 0.5, 0.5) for x in colorsys.xrange(N)]
+    hex_out = []
+    for rgb in HSV_tuples:
+        rgb = map(lambda x: int(x * 255), colorsys.hsv_to_rgb(*rgb))
+        hex_out.append("".join(map(lambda x: chr(x).encode('hex'), rgb)))
+    return hex_out
+
 
 # Define some colors
 BLACK = (0, 0, 0)
